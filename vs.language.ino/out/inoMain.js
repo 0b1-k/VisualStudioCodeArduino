5bf84e0943fd825ff4da9885006275a9bc442742
fabien <froyer67@gmail.com>
2015-10-13 10:06:34 -0700

    obsolete as of 0.9.x

--- vs.language.ino/out/inoMain.js
+++
@@ -1,8 +0,0 @@
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-'use strict';
-define(["require", "exports", './inoDef', 'monaco'], function (require, exports, languageDef, monaco) {
-    monaco.Modes.registerMonarchDefinition('ino', languageDef.language);
-});
-//# sourceMappingURL=cppMain.js.map
